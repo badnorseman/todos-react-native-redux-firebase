500498d86fc7333c7577bb2c649b62db48c228c6
Dan Jensen <urbanvikingr@gmail.com>
2016-09-22 20:39:55 -0400

    Github authentication

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,10 +1,10 @@
-import { Linking } from 'react-native'
-import Firebase from 'firebase'
+// import { Linking } from 'react-native'
+// import Firebase from 'firebase'
 import firebaseApp from '../firebaseApp'
 import * as actionTypes from '../constants/actionTypes'
 import { makeActionCreator } from '../utils/makeActionCreator'
 
-const githubConfig = require('../../githubconfig.json')
+// const githubConfig = require('../../githubconfig.json')
 
 const Todo = firebaseApp.database().ref()
 
@@ -77,11 +77,10 @@ export function deleteTodo(id) {
 }
 
 function authorize() {
-  signInAnonymously() // OK
-  signInWithGithub() // Broken
-  signInWithGithubUrl() // Wrong redirect
+  signInAnonymously()
 }
 
+// Works but we need to implement privacy for user data
 function signInAnonymously() {
   firebaseApp.auth().signInAnonymously().then(function (result) {
     return result
@@ -90,28 +89,54 @@ function signInAnonymously() {
   })
 }
 
-function signInWithGithub() {
-  const provider = new Firebase.auth.GithubAuthProvider()
-  firebaseApp.auth().signInWithRedirect(provider)
-  firebaseApp.auth().getRedirectResult().then(result => {
-    console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
-    console.log('result', result)
-    console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
-  }).catch(error => {
-    throw new Error(error)
-  })
-}
+// Only works with Firebase version 2x
+// function signInWithGithub() {
+//   const provider = new Firebase.auth.GithubAuthProvider()
+//   firebaseApp.auth().signInWithRedirect(provider)
+//   firebaseApp.auth().getRedirectResult().then(result => {
+//     console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
+//     console.log('result', result)
+//     console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
+//   }).catch(error => {
+//     throw new Error(error)
+//   })
+// }
 
-function signInWithGithubUrl() {
-  const url = githubConfig.url+
-    '?client_id='+githubConfig.clientId+
-    // '&redirect_uri='+githubConfig.redirectUri+
-    '&response_type=token'
-  Linking.openURL(url).then(result => {
-    console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
-    console.log('result', result)
-    console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
-  }).catch(error => {
-    throw new Error(error)
-  })
-}
+// Require OAuth plugin to retrieve access token
+// function signInWithGithubOAuth() {
+//   const accessToken = 'missing'
+//   const credential = new firebase.auth.GithubAuthProvider.credential(accessToken)
+//   firebaseApp.auth().signInWithCredential(credential).then(result => {
+//     console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
+//     console.log('result', result)
+//     console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
+//   }).catch(error => {
+//     throw new Error(error)
+//   })
+// }
+
+// Return login html and redirects to Firebase Project
+// function signInWithGithubUrl() {
+//   const url = githubConfig.url+'?scope=user:email&client_id='+githubConfig.clientId+
+//     '&response_type=token'
+//   fetch(url).then(result => {
+//     console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
+//     console.log('result', result)
+//     console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
+//   }).catch(error => {
+//     throw new Error(error)
+//   })
+// }
+
+// Redirects to Firebase Project
+// function signInWithGithubAndLinking() {
+//   const url = githubConfig.url+'?scope=user:email&client_id='+githubConfig.clientId+
+//     '&response_type=token'
+//   Linking.openURL(url).then(result => {
+//     console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
+//     console.log('result', result)
+//     console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
+//   }).catch(error => {
+//     throw new Error(error)
+//   })
+// }
