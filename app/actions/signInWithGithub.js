ad63e013e4173db8bf3b2b05639cefe485dbb712
Dan Jensen <urbanvikingr@gmail.com>
2016-09-23 23:59:16 -0400

    update Github oauth to retrieve token

--- app/actions/signInWithGithub.js
+++ app/actions/signInWithGithub.js
@@ -3,24 +3,25 @@ import { Linking } from 'react-native'
 const githubConfig = require('../../githubconfig.json')
 
 function getTokenFromGithub(code) {
-  const url = githubConfig.url+'access_token'
-  const request = {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      client_id: githubConfig.clientId,
-      client_secret: githubConfig.clientSecret,
-      code,
-      state: Math.random().toString(36)
-    })
-  }
+  const request = new Request(
+    githubConfig.url+'access_token', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        client_id: githubConfig.clientId,
+        client_secret: githubConfig.clientSecret,
+        code,
+        state: Math.random().toString(36)
+      })
+    }
+  )
 
-  return fetch(url, request)
+  return fetch(request)
   .then(result => result.json())
-  .then(json => json.access_token)
+  .then(resultJson => resultJson.access_token)
   .catch(error => {
     throw new Error(error)
   })
@@ -28,22 +29,22 @@ function getTokenFromGithub(code) {
 
 function handleUrl(event) {
   const code = event.url.slice(event.url.lastIndexOf('=') + 1)
-  const accessToken = getTokenFromGithub(code).done()
-  console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
-  console.log('accessToken', accessToken)
-  console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
+  getTokenFromGithub(code).done(token => {
+    console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
+    console.log('token', token)
+    console.log('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')
+  })
 
   Linking.removeEventListener('url', handleUrl)
 }
 
 export default function signInWithGithub() {
-  const url = githubConfig.url+
+  const request = githubConfig.url+
     'authorize?client_id='+githubConfig.clientId+
-    '&redirect_uri='+githubConfig.redirectUri+
-    '&response_type=token'
+    '&redirect_uri='+githubConfig.redirectUri
 
   Linking.addEventListener('url', handleUrl)
-  Linking.openURL(url).catch(error => {
+  Linking.openURL(request).catch(error => {
     throw new Error(error)
   })
 }
