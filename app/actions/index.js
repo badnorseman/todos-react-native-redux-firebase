2654ab69ca169ac104b68070a65911898cbf76fa
Dan Jensen <urbanvikingr@gmail.com>
2016-09-24 17:43:13 -0400

    remove localStorage actions from index.js

--- app/actions/index.js
+++ app/actions/index.js
@@ -5,17 +5,11 @@ import {
   fetchTodos,
   toggleTodo
 } from './todos'
-import {
-  getItemFromLocalStorage,
-  setItemToLocalStorage
-} from './localStorage'
 
 export {
   addTodo,
   deleteTodo,
   fetchTodos,
   toggleTodo,
-  getItemFromLocalStorage,
-  setItemToLocalStorage,
   setVisibilityFilter
 }
