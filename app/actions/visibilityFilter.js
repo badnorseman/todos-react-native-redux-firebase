f60ee40cde58709453a7a0f83f26e84d030e182a
Dan Jensen <urbanvikingr@gmail.com>
2016-09-25 22:29:49 -0400

    change visibilityfilter

--- app/actions/visibilityFilter.js
+++ app/actions/visibilityFilter.js
@@ -1,6 +1,6 @@
 import * as actionTypes from '../constants/actionTypes'
 import { makeActionCreator } from '../utils/makeActionCreator'
 
-const setVisibilityFilter = makeActionCreator(actionTypes.SET_VISIBILITY_FILTER, 'filter')
-
-export { setVisibilityFilter }
+export function setVisibilityFilter() {
+  return makeActionCreator(actionTypes.SET_VISIBILITY_FILTER, 'filter')
+}
